6f20eda53127ce476e51adae1438b5b366fa63e6
somename <someemail>
2017-05-12 15:36:15 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -93,6 +93,18 @@ public class TestPlanetExplorer {
 		assertEquals(expected, pe.executeCommand("f"));
 	}
 	
+	@Test
+	public void test_moving_forward_while_E_while_at_the_edge() {
+		PlanetExplorer pe = new PlanetExplorer(4, 4);
+		
+		pe.executeCommand("f");
+		pe.executeCommand("l");
+		
+		String expected = "(pos_" + 3 + ",pos_" + 1 +"," + "W" + ")";
+		
+		assertEquals(expected, pe.executeCommand("f"));
+	}
+	
 	
 	@Test
 	public void test_rotating_left_while_N(){
